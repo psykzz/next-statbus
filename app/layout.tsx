c672b2d9adce6b595b2f2c2533308d584cc4bde9
Matt Smith <matt.daemon660@gmail.com>
2022-11-26 19:37:19 +0000

    feat: add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Footer from '../components/Footer';
 import './globals.css';
 
@@ -12,6 +13,17 @@ export default function RootLayout({
       <body>
         {children}
         <Footer />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N45E21FRJB"
+        />
+        <Script id="google-gtag">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N45E21FRJB');`}
+        </Script>
       </body>
     </html>
   );
